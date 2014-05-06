62252d6daa7599f93abda52b5cad317a78e28606
Dean De Carli <neaddean@gmail.com>
2014-05-06 15:47:07 -0400

    python config script

--- src/loadconfig.py
+++ src/loadconfig.py
@@ -6,6 +6,8 @@ tdc = serial.Serial("/dev/ttyUSB0", 500000)
 csr = ["000", "020", "027", "01F", "D60", "000", "D75",
        "000", "DEB", "C0A", "A71", "E11", "1FF", "FFF"]
 
+asd_regs = ["FF", "07", "6C", "06", "02", "06", "02", "07", "55", "55", "00"]
+
 tdc.write("power_off\n")
 time.sleep(1)
 tdc.write("power_on\n")
@@ -14,8 +16,15 @@ time.sleep(1)
 for i in range(len(csr)):
     tdc.write("jtw " + format(i, "1x") + " " + csr[i] + "\n")
 
+for i in range(len(asd_regs)):
+    tdc.write("jaw " + format(i, "1x") + " " + asd_regs[i] + "\n")
+
+
 tdc.write("jtu\n")
+tdc.write("jau\n")
 tdc.write("tz\n")
 tdc.write("tc 2\n")
 tdc.write("tc 0\n")
 tdc.write("tr\n")
+
+
